11eccd7818f1c07a170d9db61707e11135e73755
DAN-MK9 <danilogith@outlook.com>
2024-05-18 18:44:09 -0300

    feat: API do formulário aplicada

---
+++ script.js
@@ -0,0 +1,53 @@
+let menu = document.getElementById("menu")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
+
+function MenuAB() {
+    if (menu.classList.contains("menu-fechado")) {
+        menu.classList.remove("menu-fechado")
+        iconeX.style.display = "inline"
+        iconeBarras.style.display = "none"
+    } else {
+        menu.classList.add("menu-fechado")
+        iconeX.style.display = "none"
+        iconeBarras.style.display = "inline"
+    }
+}
+
+window.onresize = () => {
+    menu.classList.remove("menu-fechado")
+    iconeX.style.display = "inline"
+    iconeBarras.style.display = "none"
+}
+
+const solicitarServico = (event) => {
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value 
+    let valorDescricao = document.getElementById("campo-descricao").value 
+
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    fetch("http://127.0.0.1:3000/solicitacoes", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => {
+        console.log(resposta)
+
+        document.querySelector("#contato form").reset()
+        alert("Solicitação cadastrada")
+    })
+    .catch(erro => {
+        console.error(erro)
+        alert("Erro na requisição")
+    })
+
+    event.preventDefault()
+}
